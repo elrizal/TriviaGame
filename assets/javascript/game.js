7f97dece02f2df98ebdc49b88fa977500e286c58
Erin Rizal <erinrizal@Erins-MacBook-Pro.local>
2017-12-14 23:44:42 -0500

    <c>

---
+++ assets/javascript/game.js
@@ -0,0 +1,84 @@
+
+//later, use (questions[0]) to get objects from the array
+
+var userScore = 0;
+var userEntry = 0;
+	var displayAnswers= $("#show-answers").show();
+	var hideAnswers= $("#show-answers").hide();
+
+
+window.onload = function() {
+	$("#reset").on("click", timeLimit.reset); //to be used at the end of the quiz
+	$("#start").on("click", timeLimit.start);
+	$("#stop").on("click", timeLimit.start);
+};
+
+var intervalId;
+var clockRunning = false;
+setTimeout(timeUp, 1000 * 10);
+
+var timeLimit = {
+	time: 0,
+
+	reset: function() {
+		timeLimit.time = 0;
+		$("#show-time").text("00:00");
+	},
+
+	start: function() {
+		if (!clockRunning) {
+
+			intervalId = setInterval(timeLimit.count, 1000);
+			clockRunning = true;
+		}
+	},
+	stop: function() {
+   	 clearInterval(intervalId);
+   	 clockRunning = false;
+  },
+	count: function() {
+		timeLimit.time++;
+		var converted = timeLimit.timeConverter(timeLimit.time);
+		console.log(converted);
+		$("#show-time").text(converted);
+	},
+
+	timeConverter: function(t) {
+		var minutes = Math.floor(t / 60);
+		var seconds = t - minutes * 60;
+
+		if (seconds < 10) {
+			seconds = "0" + seconds;
+		}
+
+		if (minutes === 0) {
+			minutes = "00";
+		} else if (minutes < 10) {
+			minutes = "0" + minutes;
+		}
+
+		return minutes + ":" + seconds;
+	}
+};
+
+function timeUp() {
+	timeLimit--;
+
+	console.log("done");
+
+	//$('#main-space').removeClass('status-on').addClass('status-off');
+	$("#show-quiz").empty();
+	//$(".panel panel-default").append();
+
+	alert("Time up!");
+
+	if (timeLimit === 0) {
+		stop();
+	}
+}
+//	ANSWER SELECTS:
+// may write: IF array quetions[0] === array answers [0]
+//then correct message on click.
+// if array quetions[0] === array wrongAnswers [0] then incorrect
+// must have an event trigger for the user to select options, which will then
+//be compared to the answer
